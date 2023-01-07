006523c2a7bf490951860ddadac26c6ab7ebb315
Boby <boby2014@abv.bg>
2023-01-07 13:55:11 +0200

    Add files via upload

---
+++ 01.CSharp-Basics/01.First Steps in Programming/FirstStepsInProgramming - Lab/ProjectsCreation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ProjectsCreation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string architectName = Console.ReadLine();
+            int NumberProjectsToBeMade = int.Parse(Console.ReadLine());
+            int projectTime = NumberProjectsToBeMade * 3;
+
+            if (NumberProjectsToBeMade >= 100)
+            {
+                Console.WriteLine("Can not exceed 100!");
+            }
+            else
+            {
+                Console.WriteLine($"The architect {architectName} will need {projectTime} hours to complete {NumberProjectsToBeMade} project/s.");
+            }
+        }
+    }
+}
