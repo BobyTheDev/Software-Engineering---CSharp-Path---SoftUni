b2956e9913c3fde0c4abd159eceaeb1791f7eec8
Boby <boby2014@abv.bg>
2023-01-07 13:14:32 +0200

    Add files via upload

---
+++ 01.CSharp-Basics/01.First Steps in Programming/FirstStepsInProgramming - Lab/InchToCm/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace InchToCm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double cm = double.Parse(Console.ReadLine());
+            double inch = 2.54;
+
+            double result = cm * inch;
+            Console.WriteLine(result);
+        }
+    }
+}
