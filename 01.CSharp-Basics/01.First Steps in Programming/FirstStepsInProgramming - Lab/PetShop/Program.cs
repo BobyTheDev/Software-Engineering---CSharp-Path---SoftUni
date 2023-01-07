ba0a735fd5858566c748564e5dbb5b91d0baf9f7
Boby <boby2014@abv.bg>
2023-01-07 14:09:55 +0200

    Add files via upload

---
+++ 01.CSharp-Basics/01.First Steps in Programming/FirstStepsInProgramming - Lab/PetShop/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PetShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dogFoodPackages = int.Parse(Console.ReadLine());
+            int catFoodPackages = int.Parse(Console.ReadLine());
+
+            double dogFoodPrice = 2.50;
+            int catFoodPrice = 4;
+
+            double dogExpenses = dogFoodPackages * dogFoodPrice;
+            int catExpenses = catFoodPackages * catFoodPrice;
+
+            double sum = dogExpenses + catExpenses;
+
+            Console.WriteLine($"{sum} lv.");
+
+
+        }
+    }
+}
---
+++ 01.CSharp-Basics/01.First Steps in Programming/FirstStepsInProgramming - Lab/YardGreening/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace YardGreening
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double area = int.Parse(Console.ReadLine());
+
+            if (area > 10000)
+            {
+                Console.WriteLine("The total area can't be more than 10 000 sq. m.!");
+            }
+            else
+            {
+                double price = area * 7.61;
+                double discount = price * 0.18;
+                double priceAfterDiscount = price - (discount);
+
+                Console.WriteLine($"The final price is {priceAfterDiscount} lv.");
+                Console.WriteLine($"The discount is: {discount}");
+            }
+        }
+    }
+}
