f3f890d3121174447d1be54c2c1e3ed6546fa45a
Boby <boby2014@abv.bg>
2023-01-08 12:40:21 +0200

    Add files via upload

---
+++ 01.CSharp-Basics/01.First Steps in Programming/FirstStepsInProgramming - Exercise/FishTank/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FishTank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // INPUT
+            int lenght = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            double percentage = double.Parse(Console.ReadLine());
+            // MATH FORMULAS
+            int AquariumVolume = lenght * width * height;
+            double VolumeInL = (double)AquariumVolume / 1000;
+            double NeededL = VolumeInL * (1 - percentage / 100);
+            // PRINT
+            Console.WriteLine(NeededL);
+            // WHY THIS WORKS HUHH???? NO FUCKING IDEA.
+        }
+    }
+}
---
+++ 01.CSharp-Basics/01.First Steps in Programming/FirstStepsInProgramming - Exercise/FoodDelivery/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FoodDelivery
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //PRICE LIST
+            double ChickenMenu = 10.35;
+            double FishMenu = 12.40;
+            double VeganMenu = 8.15;
+            double DeliveryFee = 2.5;
+            // INPUT SYSTEM:
+            int SumChickenMenu = int.Parse(Console.ReadLine());
+            int SumFishMenu = int.Parse(Console.ReadLine());
+            int SumVeganMenu = int.Parse(Console.ReadLine());
+            // MATHS
+            double TotalPriceChicken = SumChickenMenu * ChickenMenu;
+            double TotalPriceFish = SumFishMenu * FishMenu;
+            double TotalPriceVegan = SumVeganMenu * VeganMenu;
+            double TotalPrice = TotalPriceChicken + TotalPriceFish + TotalPriceVegan;
+            double dessert = TotalPrice * 0.2;
+            double TotalPriceForEverything = TotalPrice + dessert + DeliveryFee;
+            Console.WriteLine(TotalPriceForEverything);
+            // IDK HOW THIS SHIT WORKS BUT IT WORKS SO I WILL LEAVE IT LIKE THAT
+        }
+    }
+}
