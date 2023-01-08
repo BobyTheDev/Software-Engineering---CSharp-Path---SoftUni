a1cdc7f72e5c6b0d7b7f3689d2b144a7ed2454a9
Boby <boby2014@abv.bg>
2023-01-08 12:40:56 +0200

    Add files via upload

---
+++ 01.CSharp-Basics/01.First Steps in Programming/FirstStepsInProgramming - Exercise/BasketballEquip/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BasketballEquip
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // INPUT SYSTEM
+            int FeePerYear = int.Parse(Console.ReadLine());
+            // PRICE LIST
+            double Shoes = FeePerYear - (FeePerYear * 40/100);
+            double BsktEqip = Shoes - (Shoes * 20 / 100);
+            double Ball = (BsktEqip * 1 / 4);
+            double Accessories = (Ball * 1 / 5);
+            // TOTAL PRICE
+            double TotalPrice = FeePerYear + Shoes + BsktEqip + Ball + Accessories;
+            // PRINT
+            Console.WriteLine(TotalPrice);
+        }
+    }
+}
---
+++ 01.CSharp-Basics/01.First Steps in Programming/FirstStepsInProgramming - Exercise/DepositCalc/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DepositCalc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double depositSum = int.Parse(Console.ReadLine());
+            int depositMonths = int.Parse(Console.ReadLine());
+            double procent = double.Parse(Console.ReadLine());
+
+            double amountedTax = depositSum * procent / 100;
+            double oneMonthTax = amountedTax / 12;
+
+            double finalSum = depositSum + (depositMonths * oneMonthTax);
+
+            Console.WriteLine(finalSum);
+        }
+    }
+}
