0d151323b7323e5578e112e7914203ae3fb5c885
Boby <boby2014@abv.bg>
2023-01-12 21:56:59 +0200

    Add files via upload

---
+++ 01.CSharp-Basics/02.Conditional Statements/ConditionalStatements - Lab/AreaofFigures/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace AreaofFigures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string shape = Console.ReadLine();
+            if (shape == "square")
+            {
+                double a = double.Parse(Console.ReadLine());
+                Console.WriteLine(a * a);
+            }
+            else if (shape == "rectangle")
+            {
+                double a = double.Parse(Console.ReadLine());
+                double b = double.Parse(Console.ReadLine());
+                Console.WriteLine(a * b);
+            }
+            else if (shape == "circle")
+            {
+                double r = double.Parse(Console.ReadLine());
+                double Pi = Math.PI;
+                Console.WriteLine(Pi * (r * r));
+            }
+            else if (shape == "triangle")
+            {
+                double a = double.Parse(Console.ReadLine());
+                double h = double.Parse(Console.ReadLine());
+                Console.WriteLine(a * h / 2);
+            }
+        }
+    }
+}
