31f99dc69e72a91dffa188c9542b58f7b6c25d0f
Boby <boby2014@abv.bg>
2023-01-12 23:37:41 +0200

    Add files via upload

---
+++ 01.CSharp-Basics/02.Conditional Statements/Conditional Statements - Exercise/Time+15Minutes/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Time_15Minutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                // ЧЕТЕ ЧАС И МИНУТИ / 24:00
+                // ИЗЧИСЛЯВА КОЛКО ЩЕ БЪДЕ ЧАСЪТ СЛЕД 15 МИН
+                // ЧАС:МИНУТИ
+                // ЧАС МЕЖДУ 0 ДО 23 И МИНУТИ 0 ДО 59
+                // ВОДЕЩА НУЛА КОГАТО Е НЕОБХОДИМО
+                // 1 STEP
+                int HoursOfTheDay = int.Parse(Console.ReadLine());
+                int MinutesOfTheDay = int.Parse(Console.ReadLine());
+                // 2 STEP
+                if (MinutesOfTheDay + 15 > 59)
+                {
+                    HoursOfTheDay += 1;
+                    MinutesOfTheDay = 15 - (60 - MinutesOfTheDay);
+                }
+                else if (MinutesOfTheDay + 15 <= 59) MinutesOfTheDay += 15;
+                if (HoursOfTheDay == 24) HoursOfTheDay = 0;
+
+                if (MinutesOfTheDay <= 9)
+                {
+                    Console.WriteLine($"{HoursOfTheDay}:0{MinutesOfTheDay}");
+                }
+                else
+                {
+                    Console.WriteLine($"{HoursOfTheDay}:{MinutesOfTheDay}");
+                }
+
+                // НИЩО НЕ РАЗБИРАМ ЛОЛ
+            }
+        }
+    }
+}
---
+++ 01.CSharp-Basics/02.Conditional Statements/Conditional Statements - Exercise/ToyShop/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace ToyShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // INPUT
+            double Trip = double.Parse(Console.ReadLine());
+            int Puzzle = int.Parse(Console.ReadLine());
+            int TalkingDolls = int.Parse(Console.ReadLine());
+            int TeddyBear = int.Parse(Console.ReadLine());
+            int Minions = int.Parse(Console.ReadLine());
+            int Trucks = int.Parse(Console.ReadLine());
+            // PRICES
+            double PuzzlePrice = 2.60 * Puzzle;
+            int TalkingDollPrice = 3 * TalkingDolls;
+            double TeddyBearPrice = 4.10 * TeddyBear;
+            double MinionPrice = 8.20 * Minions;
+            int TruckPrice = 2 * Trucks;
+            //Console.WriteLine(PuzzlePrice + TalkingDollPrice + TeddyBearPrice + MinionPrice + TruckPrice); -- DEBUGGER TO TEST IF THE INPUT * PRICES DOES THE JOB (IT DOES)
+            double TotalSum = PuzzlePrice + TalkingDollPrice + TeddyBearPrice + MinionPrice + TruckPrice; // CALCULATES Total Price for the customer without discount
+            int TotalToys = Puzzle + TalkingDolls + TeddyBear + Minions + Trucks;
+            double NewTotalPrice = TotalSum;
+            if (TotalToys >= 50)
+            {
+                NewTotalPrice = TotalSum * 0.75;
+            }
+            //Console.WriteLine(NewTotalPrice);
+            double rent = NewTotalPrice * 0.1;
+            //Console.WriteLine(rent1);
+            double profit = NewTotalPrice - rent;
+            if (profit >= Trip)
+            {
+                double moneyaftertrip = profit - Trip;
+                Console.WriteLine("Yes! {0:F2} lv left.", moneyaftertrip);
+            }
+            else
+            {
+                double notenough = Trip - profit;
+                Console.WriteLine("Not enough money! {0:F2} lv needed.", notenough);
+            }
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Console.WriteLine(Trip);
+            //Console.WriteLine(profit);
+            //Console.WriteLine(notenough);
+            // DEBUGGING STUFF I USED
+        }
+    }
+}
