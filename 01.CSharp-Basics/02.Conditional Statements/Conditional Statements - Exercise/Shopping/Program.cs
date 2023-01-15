5506c8b8f844b4362875b5cfc34b2c228d1c28ba
Boby <boby2014@abv.bg>
2023-01-15 12:32:28 +0200

    Add files via upload

---
+++ 01.CSharp-Basics/02.Conditional Statements/Conditional Statements - Exercise/Shopping/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Shopping
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // PRICE
+            int video_cards = 250;
+            // INPUT
+            double PeterBudget = double.Parse(Console.ReadLine());
+            int bought_video_cards = int.Parse(Console.ReadLine());
+            int bought_cpus = int.Parse(Console.ReadLine());
+            int bought_ram = int.Parse(Console.ReadLine());
+            // Math
+            int price_cards = video_cards * bought_video_cards;
+            double price_cpus = price_cards * 0.35 * bought_cpus;
+            double price_ram = price_cards * 0.10 * bought_ram;
+            // 2ND MATH
+            double TotalPrice = price_cards + price_cpus + price_ram; 
+
+            if (bought_video_cards > bought_cpus)
+            {
+                TotalPrice = TotalPrice * 0.85;
+            }
+
+            double budgetDifference = PeterBudget - TotalPrice;
+
+
+            if (budgetDifference >= 0)
+            {
+                Console.WriteLine($"You have {budgetDifference:f2} leva left!");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money! You need {Math.Abs(budgetDifference):f2} leva more!");
+            }
+        }
+    }
+}
---
+++ 01.CSharp-Basics/02.Conditional Statements/Conditional Statements - Exercise/WorldSwimmingRecord/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace WorldSwimmingRecord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // INPUT SYSTME
+            double RecordInSec = double.Parse(Console.ReadLine());
+            double DistanceInM = double.Parse(Console.ReadLine());
+            double TimeInSec = double.Parse(Console.ReadLine());
+            // MATH
+            double timeToSwimFullDistance = DistanceInM * TimeInSec;
+            timeToSwimFullDistance = timeToSwimFullDistance + (Math.Floor((DistanceInM / 15)) * 12.5);
+            double worldRecordDifference = timeToSwimFullDistance - RecordInSec;
+            // PRINT
+            if (worldRecordDifference >= 0) 
+            {
+                Console.WriteLine($"No, he failed! He was {worldRecordDifference:f2} seconds slower.");
+            }
+            else
+            {
+                Console.WriteLine($"Yes, he succeeded! The new world record is {timeToSwimFullDistance:f2} seconds.");
+            }
+
+
+        }
+    }
+}
