941362175eb50a54eb391f5561ac2e52a2747eaf
Boby <boby2014@abv.bg>
2023-01-21 13:07:44 +0200

    Add files via upload

---
+++ 01.CSharp-Basics/03.Nested Conditional Statements/Nested Conditional Statements - Lab/FruitOrVegetable/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace FruitOrVegetable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fruitOrVegetable = Console.ReadLine();
+            switch (fruitOrVegetable)
+            {
+                    // FRUITS
+                case "banana": 
+                    Console.WriteLine("fruit"); 
+                    break; 
+                case "apple": 
+                    Console.WriteLine("fruit"); 
+                    break; 
+                case "kiwi": 
+                    Console.WriteLine("fruit"); 
+                    break; 
+                case "cherry": 
+                    Console.WriteLine("fruit"); 
+                    break;
+                case "lemon": 
+                    Console.WriteLine("fruit"); 
+                    break;
+                case "grapes": 
+                    Console.WriteLine("fruit"); 
+                    break;
+                // VEGETABLES
+                case "tomato": 
+                    Console.WriteLine("vegetable");
+                    break;
+                case "cucumber":
+                    Console.WriteLine("vegetable");
+                    break;
+                case "pepper":
+                    Console.WriteLine("vegetable");
+                    break;
+                case "carrot":
+                    Console.WriteLine("vegetable");
+                    break;
+                default: 
+                    Console.WriteLine("unknown"); 
+                    break;
+            }
+        }
+    }
+}
