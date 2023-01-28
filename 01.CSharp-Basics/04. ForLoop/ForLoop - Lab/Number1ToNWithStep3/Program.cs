f1e1de5bef8c8099a22c800c5acdcc4c5bbb54a1
Boby <boby2014@abv.bg>
2023-01-28 11:13:38 +0200

    Create Program.cs

---
+++ 01.CSharp-Basics/04. ForLoop/ForLoop - Lab/Number1ToNWithStep3/Program.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Number1ToNWithStep3
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = n; i <= 1; i += 3)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
