f02c46c9735f36d872fd100c86b2b37afef3787d
Boby <boby2014@abv.bg>
2023-09-05 17:20:33 +0300

    Add files via upload

---
+++ 01.CSharp-Basics/05.While Loop/Exam Preparation/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Exam_Preparation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Четем от конзолата брой на незадоволителни оценки, които може да получи
+            int badGrades = int.Parse(Console.ReadLine());
+
+            // 2. Създаваме променливи за:
+            //=> сумата на всички оценки
+            int sumOfGrades = 0;
+            //=>броя на всички оценки
+            int totalGrades = 0;
+            //=>брояч на незадоволителни оценки
+            int badCounter = 0;
+            //=>име на последна задача
+            string lastJob = string.Empty;
+
+            string input = Console.ReadLine();
+            int grade; // САМО ЗА WHILE ЦИКЪЛА
+            //3. Създаваме while, който се изпълянява, докато не получи съобщение "Enough"
+            while (input != "Enough")
+            {
+                //=> ако input-ът е различен от "Enough" => той е име на ПОСЛЕДНАТА задача до момента
+                lastJob = input;
+                //=> четем оценка за конкретна задача
+                grade = int.Parse(Console.ReadLine());
+                // => добавяме оценката към сумата на всички оценки до момента
+                sumOfGrades += grade;
+                // => увеличаваме брояча на всички оценки с 1
+                totalGrades++;
+                // => проверяваме тази оценка задоволителна ли е?
+                if (grade <= 4)
+                {
+                    // ==> ако не е: увеличаваме брояча на незадоволителни оценки, за да прекъсне
+                    badCounter++;
+
+                    //=> проверяваме дали е достигнал броя на незадоволителните оценки, за да прекъсне
+                    if (badCounter == badGrades)
+                    {
+                        //==> ако е: спираме изпълнението на цикъла
+                        Console.WriteLine($"You need a break, {badCounter} poor grades.");
+                        break;
+                    }
+                    //====> отпечатваме на конзолата:
+                    //====> спираме изпълнението на цикъла
+                }
+
+                //=> четем нов вход от конзолата
+                input = Console.ReadLine();
+            }
+            //4. Проверяваме дали последният input == "Enough"
+            if (input == "Enough")
+            {
+                //=> Ако е, отпечатваме: .....
+                Console.WriteLine($"Average score: {(double)sumOfGrades/totalGrades:f2}");
+                Console.WriteLine($"Number of problems: {totalGrades}");
+                Console.WriteLine($"Last problem: {lastJob}");
+            }
+        }
+    }
+}
---
+++ 01.CSharp-Basics/05.While Loop/Vacation/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Vacation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Прочитаме от конзолата
+            //=> Пари нужни за екскурзията - тип double
+            double costOfTrip = double.Parse(Console.ReadLine());
+            //=> Налични пари - тип double
+            double currentMoney = double.Parse(Console.ReadLine());
+            // 2. Създаваме променливи
+            //=> Брояч на дните - тип int
+            int daysCounter = 0;
+            //=> Брояч на харчене - тип int
+            int spendingDays = 0;
+            // 3. Създаваме while цикъл, който продължава, докато парите на Джеси са по-малко от парите,
+            //    които са й нужни за екскурзията и броячът за последователните дни е по-малък от 5.
+            while (currentMoney < costOfTrip && spendingDays < 5)
+            {
+                daysCounter++;
+                //=> Да се чете от конзолата два реда
+                //==> Първият ред текст (spend или save)
+                string action = Console.ReadLine();
+                //==> Да се чете на вторият ред парите които Джеси е спетила или похарчила
+                double dailyMoney = double.Parse(Console.ReadLine());
+                //=> Увеличаваме брояча за дни с 1
+                if (action == "save")
+                {
+                    currentMoney += dailyMoney;
+                    spendingDays = 0;
+                }
+                else if (action == "spend")
+                {
+                    currentMoney -= dailyMoney;
+                    if (currentMoney < 0) currentMoney = 0;
+                    spendingDays++;
+                    if (spendingDays > 4) break;
+                }
+            }
+
+            // 4. Проверка дали Джеси е харчила пари в пет последователни дни
+            if (spendingDays > 4) Console.WriteLine($"You can't save the money.\n{daysCounter}");
+            else Console.WriteLine($"You saved the money for {daysCounter} days.");
+        }
+    }
+}
